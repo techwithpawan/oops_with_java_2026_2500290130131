b23543534f0a8580b54fb9dff837800dbc67937a
Pawan Kumar <pawanbond1122@gmail.com>
2026-09-10 19:24:43 +0530

    first

--- oops_with_java_2026_2500290130131/src/Usecase1.java
+++ oops_with_java_2026_2500290130131/src/Usecase1.java
@@ -1,46 +1,54 @@
-public class Usecase1 {
-    public static void main(String[] args) {
-        Book b1 = new Book
-    }
-}
-
 class Book{
-    private String Title;
+    private String title;
     private String author;
-    private String price;
+    private double price;
     private String isbn;
-    final static String libaryname = "Sunrise";
-    static int bookCount  = 0;
+    static int bookCount =0;
+    final static String libraryName = "Sunrise Public Library";
 
-    Book(String t, String A, double p, String i){
-        this.Title = t;
-        this.author = a;
-        this.price = p;
-        this.isbn = i;
-        ++bookCount;
+    Book(String title, String author, double price, String isbn){
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.isbn = isbn;
+        bookCount++;
     }
-    public String getTitle(){
-        return Title;
+
+    void setTitle(String title){
+        this.title = title;
     }
-    public void setTitle(String t){
-        this.Title=t;
+    void setAuthor(String author){
+        this.author = author;
     }
-    public String getAuthor(){
+    void setPrice(double price){
+        this.price = price;
+    }
+    void setISBN(String isbn){
+        this.isbn = isbn;
+    }
+    String getTitle(){
+        return title;
+    }
+    String getAuthor(){
         return author;
     }
-    public void setAuthor(String a){
-        this.Title=a;
-    }
-    public String getPrice(){
+    double getPrice(){
         return price;
     }
-    public void setPrice(double p){
-        this.price=p;
-    }
-    public String getIsbn(){
+    String getISBN(){
         return isbn;
     }
-    public void setIsbn(String i){
-        this.isbn=i;
+
+}
+public class Usecase1 {
+    public static void main(String[] args) {
+        Book b1 = new Book("Java Basics", "J. Author", 350.0, "ISBN001");
+        Book b2 = new Book("OOP Concepts", "K. Writer", 420.0, "ISBN002");
+ 
+        System.out.println(b1.getTitle() + " by " + b1.getAuthor());
+        System.out.println("Price: " + b2.getPrice());
+    
+        System.out.println("Library: " + Book.libraryName);
+        System.out.println("Total books added: " + Book.bookCount);
     }
 }
